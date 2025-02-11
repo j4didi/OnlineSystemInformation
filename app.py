f3291cc4ceb5b4b2d29690897f71fc46ad2f076c
Moho <mohojadidi@gmail.com>
2025-02-11 17:07:44 +0330

    first push

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 from flask import Flask, jsonify, render_template
 import psutil
+import time
 
 app = Flask(__name__)
 
+prev_net_io = psutil.net_io_counters()
+prev_disk_io = psutil.disk_io_counters()
+prev_time = time.time()
+
 def get_system_info():
-    net_io = psutil.net_io_counters()
+    global prev_net_io, prev_disk_io, prev_time
+
+    current_net_io = psutil.net_io_counters()
+    current_disk_io = psutil.disk_io_counters()
+    current_time = time.time()
+
+    time_diff = current_time - prev_time
+    if time_diff == 0:
+        time_diff = 1
+
+    download_speed = (current_net_io.bytes_recv - prev_net_io.bytes_recv) / time_diff / (1024 ** 2)
+    upload_speed = (current_net_io.bytes_sent - prev_net_io.bytes_sent) / time_diff / (1024 ** 2)
+
+    read_speed = (current_disk_io.read_bytes - prev_disk_io.read_bytes) / time_diff / (1024 ** 2)
+    write_speed = (current_disk_io.write_bytes - prev_disk_io.write_bytes) / time_diff / (1024 ** 2)
+
+    prev_net_io = current_net_io
+    prev_disk_io = current_disk_io
+    prev_time = current_time
+
     return {
         "cpu_percent": psutil.cpu_percent(interval=1),
         "memory_percent": psutil.virtual_memory().percent,
-        "disk_percent": psutil.disk_usage('/').percent,
-        "network_usage": (net_io.bytes_sent + net_io.bytes_recv) / (1024**2),
+        "disk_read": round(read_speed, 2),
+        "disk_write": round(write_speed, 2),
+        "network_download": round(download_speed, 2),
+        "network_upload": round(upload_speed, 2)
     }
 
 @app.route('/')
